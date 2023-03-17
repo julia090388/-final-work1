335760b7f3a8e233638b89a4416dc93d05fc9b31
julia <bezrukova.juliko@yandex.ru>
2023-03-18 00:52:48 +0300

    array

---
+++ zadacha/Program.cs
@@ -0,0 +1,45 @@
+﻿string[] inputArray = { "hello", "2", "world", ":-)", "1234", "1567" };
+
+string PrintArray(string[] inputArray)
+{
+    return string.Join(", ", inputArray);
+}
+
+int CountStringInArray(string[] input)
+{
+    int len = inputArray.Length;
+    int count = 0;
+    int maxLengthString = 3;
+    for (int i = 0; i < len; i++)
+    {
+        if (inputArray[i].Length <= maxLengthString)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+string[] CreateNewArray(string[] inputArray, int countWords)
+{
+    int len = inputArray.Length;
+    string[] outputArray = new string[countWords];
+    int count = 0;
+    int maxLengthString = 3;
+    for (int i = 0; i < len; i++)
+    {
+        if (inputArray[i].Length <= maxLengthString)
+        {
+            outputArray[count] = inputArray[i];
+            count++;
+        }
+    }
+
+    return outputArray;
+}
+
+PrintArray(inputArray);
+
+string[] outputArray = CreateNewArray(inputArray, CountStringInArray(inputArray));
+
+PrintArray(outputArray);
